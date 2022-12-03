bf29f077228a0c4c3143e98a3426292d2d98dc58
joonhsong <99230083+joonhsong@users.noreply.github.com>
2022-12-02 18:08:23 -0600

    Add files via upload
    button update

--- button.py
+++ button.py
@@ -10,6 +10,7 @@ class Button():
 		if self.image is None:
 			self.image = self.text
 		self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
+		self.rect.height = 150
 		self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
 
 	def update(self, screen):
@@ -18,12 +19,13 @@ class Button():
 		screen.blit(self.text, self.text_rect)
 
 	def checkForInput(self, position):
-		if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
+		if position[0] in range(self.rect.left, self.rect.right) and position[1]-75 in range(self.rect.top, self.rect.bottom):
 			return True
 		return False
 
 	def changeColor(self, position):
-		if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
+		if position[0] in range(self.rect.left, self.rect.right) and position[1]-75 in range(self.rect.top, self.rect.bottom):
 			self.text = self.font.render(self.text_input, True, self.hovering_color)
+			##print(self.text_input, "TOP", self.rect.top, "BOTTOM", self.rect.bottom)
 		else:
 			self.text = self.font.render(self.text_input, True, self.base_color)
